02db36cffb5183fa05d847eff521356f51f0e4aa
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-12 14:40:24 +0800

    Create tf_crawler.py
    A simple code for crawling the information of the popular TF-lipsticks

---
+++ tf_crawler.py
@@ -0,0 +1,28 @@
+#A simple code for crawling the information of the popular TF-lipsticks
+import requests
+import re
+from bs4 import BeautifulSoup
+from requests.packages import urllib3
+urllib3.disable_warnings()
+
+url='https://www.tom-ford.cn/'
+data={}
+headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+        }
+
+response=requests.get(url,headers=headers)
+html_doc=response.content #TF
+#print(response.status_code)   #状态码
+#print(response.content.decode("utf-8")) #内容
+
+soup=BeautifulSoup(html_doc, #html文档字符串
+        'html.parser', #html解析器
+        from_encoding='utf-8' #html文档编码
+        )
+
+TF_type=soup.find_all('a',href=re.compile(r"goods-"))
+
+for tf_type in TF_type:
+    #print(tf_type.name,tf_type['href'],tf_type.get_text())
+    print(tf_type.get_text())
