743742cbaa50b32e5770aae8db6d2d50cdf89a4d
woaixiaoyuyu <38500738+woaixiaoyuyu@users.noreply.github.com>
2018-12-12 21:54:39 +0800

    Update tf_crawler.py

--- tf_crawler.py
+++ tf_crawler.py
@@ -5,21 +5,23 @@ from bs4 import BeautifulSoup
 
 url='https://www.tom-ford.cn/'
 data={}
-headers={
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/70.0.3538.77 Safari/537.36'
         }
 
-response=requests.get(url,headers=headers)
-html_doc=response.content #TF
+response = requests.get(url, headers=headers)
+html_doc = response.content  # TF
 #print(response.status_code)   #状态码
 #print(response.content.decode("utf-8")) #内容
 
-soup=BeautifulSoup(html_doc, #html文档字符串
-        'html.parser', #html解析器
-        from_encoding='utf-8' #html文档编码
-        )
+soup = BeautifulSoup(
+    html_doc,
+    'html.parser',
+    from_encoding='utf-8'  # html文档编码#
+)
 
-TF_type=soup.find_all('a',href=re.compile(r"goods-"))
+TF_type = soup.find_all('a', href=re.compile(r"goods-"))
 
 for tf_type in TF_type:
     #print(tf_type.name,tf_type['href'],tf_type.get_text())
